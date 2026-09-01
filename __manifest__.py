355ebb07dbb52ac68a3ee6d91f8e1f48eccf7d2a
Arsham Zarei <arsham.zarei1378@gmile.com>
2026-09-01 15:42:32 +0330

    start project

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': "مدرسه",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "آرشام زارعی",
+    'category': 'شخصی/آموزشی',
+    'description': """
+    تمامی اطلاعات مدرسه ، اعم از معلم ها و دانش آموزان در این قسمت قرار می گیرد
+    """,
+    'license': 'LGPL-3',
+    'application': True,
+    'installable': True,
+    'data': [
+
+            ],
+}
